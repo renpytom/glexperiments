692d7a1e5a833010e7eeb3a327b2d96c11bff501
Tom Rothamel <tom@rothamel.us>
2018-12-17 02:44:52 -0500

    Document parts of Mesh.

--- polygon.pyx
+++ polygon.pyx
@@ -299,7 +299,15 @@ cdef class Mesh:
 
     def __init__(self):
         """
-        Creates a new mesh.
+        Represents a mesh consisting of one or more polygons.
+
+        This needs to be created in the correct order.
+
+        First, use add_attribute to add attributes. Then call add_polygon
+        to add polygons with attributes. After that, it is possible to call
+        methods like offset to manipulate the mesh.
+
+        Once get_data is called (by Shader.draw), the mesh becomes immutable.
         """
 
         self.points = 0
@@ -313,10 +321,29 @@ cdef class Mesh:
             free(self.data)
 
     def add_attribute(self, name, size):
+        """
+        Adds an attribute to this mesh.
+
+        `name`
+            The name of the attribute.
+
+        `size`
+            The number of floats per vertex that make up the attribute.
+        """
+
         self.attributes[name] = self.stride
         self.stride += size
 
     def add_polygon(self, data):
+        """
+        Adds a polygon.
+
+        `data`
+            This is an iterable. It should have self.stride data for each
+            vertex, and the number of vertices is derived from the length
+            of the data.
+        """
+
         cdef Polygon p = Polygon(self.stride, len(data) // self.stride, data)
         self.points += p.points
 
@@ -343,6 +370,10 @@ cdef class Mesh:
         return self.data + <int> self.attributes[name]
 
     def copy(self):
+        """
+        Returns a copy of this mesh.
+        """
+
         rv = Mesh()
         rv.stride = self.stride
         rv.points = self.points
@@ -352,6 +383,10 @@ cdef class Mesh:
         return rv
 
     def offset(self, float x, float y, float z):
+        """
+        Applies an offset to the position of every
+        """
+
         cdef Polygon p
 
         for p in self.polygons:

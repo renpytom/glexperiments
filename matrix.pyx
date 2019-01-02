3712af8ac4c02fe2459cad6b1230e74a5d3515a7
Tom Rothamel <tom@rothamel.us>
2019-01-01 22:56:29 -0500

    Change Matrix.
    There are now accessors for the various xdx, xdy, etc style fileds.

--- matrix.pyx
+++ matrix.pyx
@@ -5,92 +5,103 @@ from libc.string cimport memset
 
 cdef class Matrix:
     """
-    Represents a `dimension` x `dimension` matrix, where 0 < `dimension` <= 5.
+    Represents a 4x4 matrix.
     """
 
-    def __init__(Matrix self, int dimension, l):
+    def __init__(Matrix self, l):
 
-        self.dimension = dimension
-
-        cdef int limit = dimension * dimension
-
-        memset(self.m, 0, sizeof(float) * limit)
+        memset(self.m, 0, sizeof(float) * 16)
 
         if l is None:
             return
 
-        cdef int i = 0
+        cdef int lenl = len(l)
 
-        for j in l:
-            self.m[i] = j
-            i += 1
+        if lenl == 4:
+            (self.xdx, self.xdy,
+             self.ydx, self.ydy) = l
+            self.ydy = 1.0
+            self.wdw = 1.0
 
-            if i == limit:
-                break
+        elif lenl == 9:
+            (self.xdx, self.xdy, self.xdz,
+             self.ydx, self.ydy, self.ydz,
+             self.zdx, self.zdy, self.zdz) = l
+            self.wdw = 1.0
+
+        elif lenl == 16:
+            (self.xdx, self.xdy, self.xdz, self.xdw,
+             self.ydx, self.ydy, self.ydz, self.ydw,
+             self.zdx, self.zdy, self.zdz, self.zdw,
+             self.wdx, self.wdy, self.wdz, self.wdw) = l
+
+        else:
+            raise Exception("Unsupported matrix length {} (must be 4, 9, or 16).".format(len(l)))
 
 
     def __mul__(Matrix self, Matrix other):
 
-        cdef float *a = self.m
-        cdef float *b = other.m
+        cdef Matrix rv = Matrix(None)
 
-        cdef int x, y, i
+        rv.xdx = other.wdx*self.xdw + other.xdx*self.xdx + other.ydx*self.xdy + other.zdx*self.xdz
+        rv.xdy = other.wdy*self.xdw + other.xdy*self.xdx + other.ydy*self.xdy + other.zdy*self.xdz
+        rv.xdz = other.wdz*self.xdw + other.xdz*self.xdx + other.ydz*self.xdy + other.zdz*self.xdz
+        rv.xdw = other.wdw*self.xdw + other.xdw*self.xdx + other.ydw*self.xdy + other.zdw*self.xdz
 
-        cdef int d = self.dimension
+        rv.ydx = other.wdx*self.ydw + other.xdx*self.ydx + other.ydx*self.ydy + other.zdx*self.ydz
+        rv.ydy = other.wdy*self.ydw + other.xdy*self.ydx + other.ydy*self.ydy + other.zdy*self.ydz
+        rv.ydz = other.wdz*self.ydw + other.xdz*self.ydx + other.ydz*self.ydy + other.zdz*self.ydz
+        rv.ydw = other.wdw*self.ydw + other.xdw*self.ydx + other.ydw*self.ydy + other.zdw*self.ydz
 
-        cdef Matrix rv = Matrix(self.dimension, None)
+        rv.zdx = other.wdx*self.zdw + other.xdx*self.zdx + other.ydx*self.zdy + other.zdx*self.zdz
+        rv.zdy = other.wdy*self.zdw + other.xdy*self.zdx + other.ydy*self.zdy + other.zdy*self.zdz
+        rv.zdz = other.wdz*self.zdw + other.xdz*self.zdx + other.ydz*self.zdy + other.zdz*self.zdz
+        rv.zdw = other.wdw*self.zdw + other.xdw*self.zdx + other.ydw*self.zdy + other.zdw*self.zdz
 
-        for 0 <= y < self.dimension:
-            for 0 <= x < self.dimension:
-                rv.m[x + y * d] = 0.0
-                for 0 <= i < self.dimension:
-                    rv.m[x + y * d] += a[x + i * d] * b[i + y * d]
+        rv.wdx = other.wdx*self.wdw + other.xdx*self.wdx + other.ydx*self.wdy + other.zdx*self.wdz
+        rv.wdy = other.wdy*self.wdw + other.xdy*self.wdx + other.ydy*self.wdy + other.zdy*self.wdz
+        rv.wdz = other.wdz*self.wdw + other.xdz*self.wdx + other.ydz*self.wdy + other.zdz*self.wdz
+        rv.wdw = other.wdw*self.wdw + other.xdw*self.wdx + other.ydw*self.wdy + other.zdw*self.wdz
 
         return rv
 
-
     def __getitem__(Matrix self, int index):
-        if (index < 0) or (index >= self.dimension * self.dimension):
-            raise IndexError("Matrix index out of range.")
+        if 0 <= index < 16:
+            return self.m[index]
 
-        return self.m[index]
+        raise IndexError("Matrix index out of range.")
 
     def __setitem__(Matrix self, int index, float value):
-        if (index < 0) or (index >= self.dimension * self.dimension):
-            raise IndexError("Matrix index out of range.")
+        if 0 <= index < 16:
+            self.m[index] = value
+            return
 
-        self.m[index] = value
+        raise IndexError("Matrix index out of range.")
 
-    def __repr__(self):
-
+    def __repr__(Matrix self):
         cdef int x, y
-        cdef int d = self.dimension
 
         rv = "Matrix(["
 
-        for 0 <= y < d:
+        for 0 <= y < 4:
             if y:
                 rv += "\n        "
-            for 0 <= x < d:
-                rv += "{:8.5f}, ".format(self.m[x + y * d])
+            for 0 <= x < 4:
+                rv += "{:8.5f}, ".format(self.m[x + y * 4])
 
         return rv + "])"
 
-    def apply(self, x, y, z=0.0, w=1.0):
-        """
-        Applies this matrix to a vertex, and returns the transformed
-        point. (This is more intended for debug usage, as it can be slow
-        for normal use.)
-        """
+    def transform(Matrix self, float x, float y, float z=0.0, float w=1.0, int components=2):
+        cdef float ox, oy, oz, ow
 
-        cdef float *m = self.m
+        self.transform4(&ox, &oy, &oz, &ow, x, y, z, w)
 
-        return (
-            x * m[0] + y * m[1] + z * m[2] + w * m[3],
-            x * m[4] + y * m[5] + z * m[6] + w * m[7],
-            x * m[8] + y * m[9] + z * m[10] + w * m[11],
-            x * m[12] + y * m[13] + z * m[14] + w * m[15],
-            )
+        if components == 2:
+            return (ox, oy)
+        elif components == 3:
+            return (ox, oy, oz)
+        elif components == 4:
+            return (ox, oy, oz, ow)
 
 
 def identity_matrix(int dimension):
@@ -98,17 +109,17 @@ def identity_matrix(int dimension):
     Returns a `dimension` x `dimension` identity matrix.
     """
 
-    cdef Matrix rv = Matrix(dimension, None)
+    cdef Matrix rv = Matrix(None)
 
-    cdef int i
-
-    for 0 <= i < dimension:
-        rv.m[i * dimension + i] = 1.0
+    rv.xdx = 1.0
+    rv.ydy = 1.0
+    rv.zdz = 1.0
+    rv.wdw = 1.0
 
     return rv
 
 
-def renpy_projection_matrix(w, h, n, p, f):
+def renpy_projection_matrix(float w, float h, float n, float p, float f):
     """
     Returns the Ren'Py projection matrix. This is a view into a 3d space
     where (0, 0) is the top left corner (`w`/2, `h`/2) is the center, and
@@ -128,25 +139,28 @@ def renpy_projection_matrix(w, h, n, p, f):
         The distance of the far plane from the camera.
     """
 
-    w *= 1.0
-    h *= 1.0
-    n *= 1.0
-    p *= 1.0
-    f *= 1.0
+    # This is the combination of two matrices multiplied together:
 
-    offset = Matrix(4, [
-        1.0, 0.0, 0.0, -w / 2.0,
-        0.0, 1.0, 0.0, -h / 2.0,
-        0.0, 0.0, 1.0, -p,
-        0.0, 0.0, 0.0, 1.0,
-        ])
+    # The offset matrix:
+    #    1.0, 0.0, 0.0, -w / 2.0
+    #    0.0, 1.0, 0.0, -h / 2.0
+    #    0.0, 0.0, 1.0, -p
+    #    0.0, 0.0, 0.0, 1.0
 
-    # Projection. Note how this inverts y as well.
-    project = Matrix(4, [
-        2.0 * p / w, 0, 0, 0,
-        0, -2.0 * p / h, 0, 0.0,
-        0, 0, -(f+n)/(f-n), -2 * f * n / (f - n),
-        0, 0, -1.0, 0,
-        ])
+    # The projection matrix:
+    #    2.0 * p / w,          0.0,          0.0,                 0.0
+    #            0.0, -2.0 * p / h,          0.0,                 0.0
+    #            0.0,          0.0, -(f+n)/(f-n), -2 * f * n / (f - n)
+    #            0.0,          0.0,         -1.0,                 0.0,
 
-    return offset * project
+    rv = Matrix(None)
+
+    rv.xdx = 2.0*p/w
+    rv.xdz = 0.5*w
+    rv.ydy = -2.0*p/h
+    rv.ydz = 0.5*h
+    rv.zdz = 1.0*p + 1.0*(-f - n)/(f - n)
+    rv.zdw = -2.0*f*n/(f - n)
+    rv.wdz = -1.00000000000000
+
+    return rv
--- symbolic_matrix.py
+++ symbolic_matrix.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from sympy import symbols, Matrix
 
 
@@ -34,4 +35,78 @@ def generate_rotations():
         print("rv.m[{}] = {}".format(i, expr))
 
 
-generate_rotations()
+matrix_names = [
+    "xdx",
+    "xdy",
+    "xdz",
+    "xdw",
+
+    "ydx",
+    "ydy",
+    "ydz",
+    "ydw",
+
+    "zdx",
+    "zdy",
+    "zdz",
+    "zdw",
+
+    "wdx",
+    "wdy",
+    "wdz",
+    "wdw",
+    ]
+
+
+def prefixed_matrix(prefix):
+    """
+    Returns a matrix where each entry is of the for prefix___name.
+    """
+
+    return Matrix(4, 4, [ symbols(prefix + "___" + i) for i in matrix_names ])
+
+
+def print_matrix(m):
+
+    print()
+
+    for name, value in zip(matrix_names, m):
+        if value == 0.0:
+            continue
+
+        print("rv.{} =".format(name), str(value).replace("___", "."))
+
+
+def matrix_mult():
+    print()
+
+    multiplied = prefixed_matrix("self") * prefixed_matrix("other")
+
+    for name, value in zip(matrix_names, multiplied):
+        print("rv.{} =".format(name), str(value).replace("___", "."))
+
+
+def renpy_projection_matrix():
+
+    w, h, n, p, f = symbols('w h n p f')
+
+    offset = Matrix(4, 4, [
+        1.0, 0.0, 0.0, -w / 2.0,
+        0.0, 1.0, 0.0, -h / 2.0,
+        0.0, 0.0, 1.0, -p,
+        0.0, 0.0, 0.0, 1.0,
+    ])
+
+    projection = Matrix(4, 4, [
+        2.0 * p / w, 0.0, 0.0, 0.0,
+        0.0, -2.0 * p / h, 0.0, 0.0,
+        0.0, 0.0, -(f+n)/(f-n), -2 * f * n / (f - n),
+        0.0, 0.0, -1.0, 0.0,
+    ])
+
+    print_matrix(offset)
+    print_matrix(projection)
+    print_matrix(offset * projection)
+
+
+renpy_projection_matrix()

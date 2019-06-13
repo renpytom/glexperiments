aca6203520dacc3f15424698f2f6c3fbc0d8b12a
Tom Rothamel <tom@rothamel.us>
2019-06-12 22:03:08 -0400

    Only for mag.

--- ftl.pyx
+++ ftl.pyx
@@ -162,7 +162,7 @@ cpdef GLuint load_texture(fn):
     glDeleteTextures(1, &tex)
 
     glBindTexture(GL_TEXTURE_2D, premultiplied)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
